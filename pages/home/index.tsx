f0523f7769cd596fbb7baa07e05b74922e2d3b36
Alaister Young <a@alaisteryoung.com>
2023-08-08 21:03:02 +1000

    adds og image

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -6,14 +6,24 @@ import HomeNav from '~/components/home/nav'
 import Pricing from '~/components/home/pricing'
 import Support from '~/components/home/support'
 
+const title = 'side.domains — Sales pages for your side project domains'
+const description =
+  'Receive offers on your side project domains with simple sales pages. No commissions, no subscriptions. Secured with SSL/TLS.'
+
 const HomePage = () => {
   return (
     <>
       <Head>
-        <title>side.domains — Sales pages for your side project domains</title>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:creator" content="@alaisteryoung" />
+        <meta property="og:url" content="https://side.domains/" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
         <meta
-          name="description"
-          content="Receive offers on your side project domains with simple sales pages. No commissions, no subscriptions. Secured with SSL/TLS."
+          property="og:image"
+          content="https://side.domains/side-domains-og.png"
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
