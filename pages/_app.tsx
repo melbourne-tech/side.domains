6f450dbca82a39dde6074d320b7088a2a948ed8c
Alaister Young <alaister@users.noreply.github.com>
2024-10-28 18:52:19 +0800

    feat: add fathom analytics (#15)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,8 @@
 import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import * as Fathom from 'fathom-client'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 import { Toaster } from '~/components/ui/toaster'
 import { AuthContextProvider } from '~/lib/contexts/auth'
 import { PreloadedContextProvider } from '~/lib/contexts/preloaded'
@@ -9,7 +12,29 @@ import { AppPropsWithLayout } from '~/lib/types'
 import { identity } from '~/lib/void'
 
 function CustomApp({ Component, pageProps }: AppPropsWithLayout) {
+  const router = useRouter()
   const queryClient = useRootQueryClient()
+
+  useEffect(() => {
+    if (!process.env.NEXT_PUBLIC_FATHOM_SITE_ID) {
+      return
+    }
+
+    Fathom.load(process.env.NEXT_PUBLIC_FATHOM_SITE_ID, {
+      includedDomains: ['side.domains', 'www.side.domains', 'app.side.domains'],
+    })
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview()
+    }
+
+    router.events.on('routeChangeComplete', onRouteChangeComplete)
+
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   const getLayout = Component.getLayout ?? identity
 
